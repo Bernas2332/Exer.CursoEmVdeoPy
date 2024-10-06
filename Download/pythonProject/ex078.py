adb2956b23db1e44dc42c03baeb33bdc1690d186
Bernas2332 <bernardoalmeida638@gmail.com>
2024-10-06 18:23:19 -0300

    Update ex078.py

--- Download/pythonProject/ex078.py
+++ Download/pythonProject/ex078.py
@@ -1,4 +1,13 @@
 lista = []
+maior = 0
+menor = 0
 for num in range(0,5):
     lista.append(int(input('Digite um número ')))
-print 
+    if num == 0:
+        maior = menor = lista[num]
+    else:
+        if maior < lista[num]:
+            maior = lista[num]
+        if menor > lista[num]:
+            menor = lista[num]
+print(f'O maior é {maior} e o menor {menor}')
